e42a83e5e813f37e82d62737bd2d06f5383f113b
Dario <dcindries@gmail.com>
2026-05-14 18:27:40 +0200

    Increase image upload limit and add streak badges

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,15 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "res.cloudinary.com"
-      }
-    ]
-  }
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "6mb",
+    },
+  },
 };
 
 export default nextConfig;
